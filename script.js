fed030c510758406264756b2d11939b54180f419
KoifishVEVO <149301244+KoifishVEVO@users.noreply.github.com>
2025-06-13 10:13:18 +0700

    perbaiki tes yang gagal

--- script.js
+++ script.js
@@ -29,6 +29,47 @@ display.value = '';
 
 }
 
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+console.log("tes");
+
 
 function deleteLast() {
 
